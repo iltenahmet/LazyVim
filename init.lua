acac0efae304f1a6e41636bb0c67c388344a4875
iltenahmet <iltenahmet@gmail.com>
2024-11-27 16:29:40 -0500

    colorscheme

--- init.lua
+++ init.lua
@@ -25,7 +25,38 @@ require("gruvbox").setup({
   dim_inactive = false,
   transparent_mode = false,
 })
-vim.cmd("colorscheme github_dark_default")
+
+local c = require('vscode.colors').get_colors()
+require('vscode').setup({
+    -- Alternatively set style in setup
+    -- style = 'light'
+
+    -- Enable transparent background
+    transparent = true,
+
+    -- Enable italic comment
+    italic_comments = false,
+
+    -- Underline `@markup.link.*` variants
+    underline_links = true,
+
+    -- Disable nvim-tree background color
+    disable_nvimtree_bg = false,
+
+    -- Override colors (see ./lua/vscode/colors.lua)
+    color_overrides = {
+        -- vscLineNumber = '#FFFFFF',
+    },
+
+    -- Override highlight groups (see ./lua/vscode/theme.lua)
+    group_overrides = {
+        -- this supports the same val table as vim.api.nvim_set_hl
+        -- use colors from this colorscheme by requiring vscode.colors!
+        Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+    }
+})
+
+vim.cmd("colorscheme vscode")
 
 vim.g.autoformat = false
 vim.cmd("set nornu")
