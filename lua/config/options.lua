ce476e776a68541eb56c450b615e354dd976b92a
iltenahmet <iltenahmet@gmail.com>
2026-04-03 12:45:34 -0400

    supress warnings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,4 @@
 
 vim.g.autoformat = false
 vim.g.root_spec = { "cwd" }
+
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -2,6 +2,15 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      diagnostics = {
+        virtual_text = false,
+        signs = {
+          severity = { min = vim.diagnostic.severity.ERROR },
+        },
+        underline = {
+          severity = { min = vim.diagnostic.severity.ERROR },
+        },
+      },
       servers = {
         vtsls = {
           root_dir = vim.fn.getcwd(),
